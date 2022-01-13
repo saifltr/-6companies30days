4b8a5050f56481af99fa7823a196497477cffce1
Saif Bagmaru <83292987+saifltr@users.noreply.github.com>
2022-01-13 14:06:57 +0530

    Problem 15: Delete N nodes after M nodes of a Linked list

---
+++ Amazon/Problem15_Delete_Nodes.cpp
@@ -0,0 +1,98 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+struct Node *start = NULL;
+
+void printList(struct Node *node)
+{
+    while(node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+    
+}
+
+void insert(int n1)
+{
+    int n,value;
+    n=n1;
+    struct Node *temp;
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>value;
+        if(i==0)
+        {
+            start = new Node(value);
+            temp=start;
+            continue;
+        }
+        else
+        {
+            temp->next = new Node(value);
+            temp=temp->next;
+        }
+    }
+}
+
+class Solution
+{
+    public:
+    void linkdelete(struct Node  *head, int M, int N)
+    {
+       if(head == nullptr){
+            return;
+        }
+        int ctr = 0; 
+        while(head != nullptr and head->next){
+            ctr++;
+            if(ctr == M){
+                // you have to delete here N nodes
+                Node* curr = head;
+                int loop = N;
+                while(loop--){
+                    if(curr->next)
+                        curr = curr->next;
+                }
+                head->next = curr->next;
+                ctr = 0;
+            }
+            head = head->next;
+        }
+    }
+};
+
+
+int main()
+{
+    int t,n1;
+    cin>>t;
+    while (t--) {
+        cin>>n1;
+        int m,n;
+        cin>>m;
+        cin>>n;
+        insert(n1);
+        Solution ob;
+        ob.linkdelete(start,m,n);
+        printList(start);
+    }
+    
+    return 0;
+}
