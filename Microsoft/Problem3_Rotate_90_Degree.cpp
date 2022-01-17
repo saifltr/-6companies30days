bc6f17b25ac7fa8220df0f3f2f5a261d1d2e0bc3
Saif Bagmaru <83292987+saifltr@users.noreply.github.com>
2022-01-17 11:09:56 +0530

    Problem 3: Rotate by 90 Degree

---
+++ Microsoft/Problem3_Rotate_90_Degree.cpp
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+void rotate(int n,int a[][n])
+{
+   //code here
+   for(int i=0;i<n;i++)
+   {
+       for(int j=0;j<n/2;j++)
+       {
+           int temp=a[i][j];
+           a[i][j]=a[i][n-j-1];
+           a[i][n-j-1]=temp;
+       }
+   }
+   for(int i=0;i<n;i++)
+   {
+       for(int j=0;j<i;j++)
+       {
+           int temp=a[i][j];
+           a[i][j]=a[j][i];
+           a[j][i]=temp;
+       }
+   }
+}
+
+
+
+int main()
+{
+    int t;
+    scanf("%d",&t); 
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        int a[n][n];
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+                scanf("%d",&a[i][j]);
+        }
+        rotate(n,a);
+        for (int i = 0; i < n; ++i)
+        {
+            for(int j=0; j<n; j++)
+                printf("%d ",a[i][j]);
+            printf("\n");
+        }
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
